ae1d5b7eef07eefe4b9dadd04cc2fb22cdadb763
Tong <1427275295@qq.com>
2025-08-30 05:51:56 +0800

    add simple Zod validation to SearchFile component; use Fortawesome to replace words with icons in SearchFile component

--- src/components/SearchFile.js
+++ src/components/SearchFile.js
@@ -1,5 +1,13 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { z } from 'zod';
+
+const SearchFilePropsSchema = z.object({
+    title: z.string().default('文档'),
+    onSearch: z.function()
+});
 
 const Searchdiv = styled.div.attrs({
     className: "d-flex align-items-center justify-content-between"
@@ -17,35 +25,38 @@ const Searchdiv = styled.div.attrs({
     }
 `
 
-export default function SearchFile({ title, onSearch }) {
+export default function SearchFile(props) {
     const [searchActive, setSearchActive] = useState(false);
     const [value, setValue] = useState('');
     const inputRef = useRef(null);
+
+    // 验证 props
+    const { title, onSearch } = SearchFilePropsSchema.parse(props);
 
     const closeSearch = () => {
         setSearchActive(false);
         setValue('');
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         const searchHandler = (key) => {
-            const {keyCode} = key;
-            if(keyCode === 13 && searchActive){
+            const { keyCode } = key;
+            if (keyCode === 13 && searchActive) {
                 onSearch(value);
             }
-            if(keyCode === 27 && searchActive){
+            if (keyCode === 27 && searchActive) {
                 closeSearch();
             }
         }
         document.addEventListener('keyup', searchHandler);
-        
-        return ()=>{
+
+        return () => {
             document.removeEventListener('keyup', searchHandler);
         }
     });
 
-    useEffect(()=>{
-        if(searchActive){
+    useEffect(() => {
+        if (searchActive) {
             inputRef.current.focus();
         }
     });
@@ -57,7 +68,9 @@ export default function SearchFile({ title, onSearch }) {
                 <>
                     <Searchdiv>
                         <span>{title}</span>
-                        <span onClick={() => { setSearchActive(true) }}>搜索</span>
+                        <span onClick={() => setSearchActive(true)}>
+                            <FontAwesomeIcon icon={faSearch} color="#fff" />
+                        </span>
                     </Searchdiv>
                 </>
             }
@@ -65,8 +78,10 @@ export default function SearchFile({ title, onSearch }) {
                 searchActive &&
                 <>
                     <Searchdiv>
-                        <input ref={inputRef} value={value} onChange={(e)=>{setValue(e.target.value)}}/>
-                        <span onClick={ closeSearch }>关闭</span>
+                        <input ref={inputRef} value={value} onChange={(e) => { setValue(e.target.value) }} />
+                        <span onClick={closeSearch}>
+                            <FontAwesomeIcon icon={faTimes} color="#fff" />
+                        </span>
                     </Searchdiv>
                 </>
             }
