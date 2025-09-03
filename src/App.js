e058efb786ac8ba85d309acb302295f2370a0413
Tong <1427275295@qq.com>
2025-09-04 04:47:24 +0800

    add states for recording files in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled from 'styled-components';
 import './App.css';
@@ -51,25 +51,49 @@ const RightDiv = styled.div.attrs({
   className: 'col-9 right-panel'
 })`
   background-color: #c9d8cd;
-  padding: 0
+  padding: 0;
+  .init-page{
+    color: #888;
+    text-align: center;
+    font: normal 28px/300px '微软雅黑'
+  }
 `;
 
 
 function App() {
+  const [files, setFiles] = useState(mockList);  //代表所有的文件信息
+  const [activeId, setActiveId] = useState(''); // 当前正在编辑的文件id
+  const [openIds, setOpenIds] = useState([]); // 当前打开的所有文件id
+  const [unSaveIds, setUnSaveIds] = useState([]); // 当前未保存的所有文件id
+
+  // 已打开的所有文件信息
+  const openFiles = files.filter(item => openIds.includes(item.id));
+  // 正编辑的文件信息
+  const activeFile = files.find(item => item.id === activeId);
+
   return (
     <div className='App container-fulid px-0'>
       <div className='row no-gutters'>
         <LeftDiv>
           <SearchFile title={"我的文档"} onSearch={(value) => { console.log(value) }}></SearchFile>
-          <FileList fileList={mockList} editFile={(id) => { console.log(id) }} saveFile={(id, value) => { console.log(id, value) }} deleteFile={(id) => { console.log(id) }}></FileList>
+          <FileList fileList={files} editFile={(id) => { console.log(id) }} saveFile={(id, value) => { console.log(id, value) }} deleteFile={(id) => { console.log(id) }}></FileList>
           <div className='btn_list'>
             <ButtonItem title={'新建'} icon={faPlus} />
             <ButtonItem title={'导入'} icon={faFileImport} />
           </div>
         </LeftDiv>
         <RightDiv>
-          <TabList files={mockList} activeItem='1' unSaveItems={['1']} clickItem={(id) => { console.log(id) }} closeItem={(id) => { console.log(id) }}></TabList>
-          <SimpleMdeReact id='cutome-id' onChange={(value) => { console.log(value) }} value={mockList[0].body} options={{ autofocus: true, spellChecker: false, minHeight: '500px' }} />
+          {
+            activeFile &&
+            <>
+              <TabList files={openFiles} activeItem={activeId} unSaveItems={unSaveIds} clickItem={(id) => { console.log(id) }} closeItem={(id) => { console.log(id) }}></TabList>
+              <SimpleMdeReact id='cutome-id' onChange={(value) => { console.log(value) }} value={activeFile.body} options={{ autofocus: true, spellChecker: false, minHeight: '500px' }} />
+            </>
+          }
+          {
+            !activeFile &&
+            <div className='init-page'>新建或打开文件</div>
+          }
         </RightDiv>
       </div>
     </div>
